64f0ae4819b23366654a7fc7aabb57b33f63bf20
guistetter <guilherme_stetter@hotmail.com>
2020-05-30 15:33:22 -0300

    criando rotas privadas, substituindo Route do react-router-dom por component Route proprio

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch /*, Route*/ } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SingUp";
 import Dashboard from "../pages/Dashboard";
 import Profile from "../pages/Profile";
+import Route from "./Route"; //Nosso componenet Route substitui react-router-dom assim temos nossa rota privada
 export default function Routes() {
   return (
     <Switch>
