5de5cc83b6ac3dead531a63db9fde9bc3e8002f8
guistetter <guilherme_stetter@hotmail.com>
2020-06-03 14:51:27 -0300

    autenticação do usuario

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,17 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Form, Input } from "@rocketseat/unform";
 import * as Yup from "yup";
+import { useDispatch } from "react-redux";
 import logo from "../../assets/logo.svg";
+import { signInRequest } from "../../store/modules/auth/actions";
+
 const schema = Yup.object().shape({
   email: Yup.string()
     .email("Insira um email valido")
     .required("O email é obrigatorio"),
   password: Yup.string().required("A senha é obrigatória"),
 });
+
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+    //console.tron.log({ email, password });
   }
+
   return (
     <>
       <img src={logo} alt="GoBarberWeb" />
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -4,13 +4,14 @@ import { Route, Redirect } from "react-router-dom";
 
 import AuthLayout from "../pages/_layouts/auth/auth";
 import DefaultLayout from "../pages/_layouts/default/default";
+import store from "../store";
 
 export default function RouteWrapper({
   component: Component,
   isPrivate = false,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
   }
