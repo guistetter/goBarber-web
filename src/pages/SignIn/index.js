cfc3f674cf500275d5dc7ce8552a8f892a481f1c
guistetter <guilherme_stetter@hotmail.com>
2020-06-03 11:38:18 -0300

    validações de signIn e signUp com lib Yup - padrao schema validation, estilizando msg de erro na pagina de autenticacao

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 import logo from "../../assets/logo.svg";
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Insira um email valido")
+    .required("O email é obrigatorio"),
+  password: Yup.string().required("A senha é obrigatória"),
+});
 export default function SignIn() {
   function handleSubmit(data) {
     console.tron.log(data);
@@ -9,7 +16,7 @@ export default function SignIn() {
   return (
     <>
       <img src={logo} alt="GoBarberWeb" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Acessar</button>
--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 import logo from "../../assets/logo.svg";
+const schema = Yup.object().shape({
+  name: Yup.string().required("O nome é obrigatorio"),
+  email: Yup.string()
+    .email("Insira um email válido")
+    .required("O Email é obrigatorio"),
+  password: Yup.string()
+    .min(6, "A senha precisa ter 6 caractes no mínimo")
+    .required("A senha é obrigatória"),
+});
 export default function SignUp() {
   function handleSubmit(data) {
     console.tron.log(data);
+    console.log(data);
   }
   return (
     <>
       <img src={logo} alt="GoBarberWeb" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" type="text" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
